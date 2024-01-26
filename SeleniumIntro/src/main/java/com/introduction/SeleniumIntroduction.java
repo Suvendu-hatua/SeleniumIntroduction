550f071ddd753b963cbeb3b813b132a768ba1256
Suvendu-hatua <suvenduhaua01@gmail.com>
2024-01-27 00:22:57 +0530

    Initial Commit

---
+++ SeleniumIntro/src/main/java/com/introduction/SeleniumIntroduction.java
@@ -0,0 +1,34 @@
+package com.introduction;
+
+import org.openqa.selenium.InsecureCertificateException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SeleniumIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hello");
+
+		//Microsoft Edge
+
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\GEZ4V7X\\Desktop\\Selenium Tutorial\\msedgedriver.exe");
+		
+		//creating instance of webdriver
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		
+		//title of the web page
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		//current url of the web page
+		String currentURL=driver.getCurrentUrl();
+		System.out.println(currentURL);
+		
+//		driver.close();
+		driver.quit();		
+	}
+
+}
